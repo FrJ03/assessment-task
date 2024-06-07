15374482a9d4cccf877261819d220732e3d80c5c
Yo <franjomor@gmail.com>
2024-06-07 18:14:56 +0200

    feat: decision and reasons box added

--- src/view/mainWindow/mainWindow.py
+++ src/view/mainWindow/mainWindow.py
@@ -7,6 +7,7 @@ from PyQt5.QtWidgets import (
 )
 from view.footer.footer import ( Footer )
 from view.cases.cases import ( Cases )
+from view.textBlock.textBlock import ( TextBlock )
 
 class MainWindow(QWidget):
     def __init__(self, cases):
@@ -15,10 +16,13 @@ class MainWindow(QWidget):
         layout = QVBoxLayout()
         
         self.cases = Cases(cases)
+        self.decision = TextBlock('Decisión')
+        self.reasoner = TextBlock('Razonamiento')
         self.footer = Footer()
 
         layout.addWidget(self.cases)
-        layout.addStretch(1)
+        layout.addWidget(self.decision)
+        layout.addWidget(self.reasoner)
         layout.addWidget(self.footer)
 
         self.setLayout(layout)
---
+++ src/view/textBlock/textBlock.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import (
+    QWidget,
+    QTextEdit,
+    QVBoxLayout,
+    QLabel
+)
+
+class TextBlock(QWidget):
+    def __init__(self, header):
+        super(TextBlock, self).__init__()
+
+        self.label = QLabel(header)
+
+        self.content = QTextEdit()
+        self.content.setReadOnly(True)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.content)
+
+        self.setLayout(layout)
