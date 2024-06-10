f2929e6d53fb2f570e234376a09efccda03cd51a
Yo <franjomor@gmail.com>
2024-06-10 17:50:38 +0200

    feat: decision for loans domain

---
+++ src/knowledgeBase/loansDomain/loansDomain.py
@@ -0,0 +1,21 @@
+from knowledgeBase.commons.decision import Decision
+
+"""Decision class for loans domain
+"""
+class LoanDecision(Decision):
+    """Decision class construtor
+
+    Args:
+        decision (bool, optional): Value that represents the decision. Defaults to False.
+    """
+    def __init__(self, decision: bool = False):
+        super(self, LoanDecision).__init__(decision)
+    """Overwrites toString method from Decision class
+    """
+    def toString(self):
+        if(self._decision):
+            message = 'Decisión: Concedidido\n\n'
+            for key, value in self._details:
+                message += f'\t{key}: {value}\n'
+        else:
+            return 'Decisión: Denegado'
