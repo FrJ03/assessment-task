5a8811b1f346d383a537ff2948d86d79a7885f01
Yo <franjomor@gmail.com>
2024-06-10 18:19:09 +0200

    feat: evaluar event

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8
+
+from knowledgeBase.commons.decision import Decision
+
 class CaseTest:
     def __init__(self, name, type, allowedValues=None):
         self.name = name
@@ -15,3 +20,32 @@ class Controller():
     
     def cases(self):
         return cases
+    
+    def eventEvaluar(self, caseData):
+        """event that evaluates the case
+
+        Args:
+            caseData (Array<pair>): Case information
+
+        Returns:
+            str: decision
+            str: explanation
+        """
+        criterios = [] #obtener criterios del caso (caso)
+        decision: Decision = Decision()#decision vacía
+        explanations = ''
+        i = 0
+
+        while(decision.getDecisionMade() == False and len(criterios) < i):
+            criterio= ''#seleccionar criterio(criterios, i)
+            valor, explanation = 0 #evaluar criterio (caso, criterio)
+            explanations += f'{explanation}\n'
+            decision = Decision()#equiparar (valor)
+            i += 1
+        
+        #If the case has pass the loop without a decision made, the decision is true
+        if(decision.getDecisionMade() == False):
+            decision.setDecision(True)
+            decision.setDecisionMade(True)
+        
+        return decision.toString(), explanations
