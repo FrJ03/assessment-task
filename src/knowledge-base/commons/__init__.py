bcbcf792ee40afb9a1a630d25a796ea6f8ffa6dc
Yo <franjomor@gmail.com>
2024-06-10 12:50:35 +0200

    feat: decision abstract class

---
+++ src/knowledge-base/commons/__init__.py
@@ -0,0 +1,9 @@
+class Decision():
+    def __init__(self, decision: bool = False):
+        self._decision = decision
+    def get(self):
+        return self._decision
+    def set(self, decision: bool):
+        self._decision = decision
+    def toString(self):
+        pass
