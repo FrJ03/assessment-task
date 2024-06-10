c31be981a1f008784d80110f8d022a67c2f486d5
Yo <franjomor@gmail.com>
2024-06-10 17:31:31 +0200

    feat: document decision module

---
+++ src/knowledgeBase/commons/decision.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8
+
+'''
+Class that represents the decision and its details
+'''
+class Decision():
+    def __init__(self, decision: bool = False):
+        """Decision class construtor
+
+        Args:
+            decision (bool, optional): Value that represents the decision. Defaults to False.
+        """
+        self._decision = decision
+        self._decisionMade = False
+        self._details = []
+    def getDecision(self):
+        """Returns the status of the decision
+
+        Returns:
+            bool: decision status
+        """
+        return self._decision
+    def setDecision(self, decision: bool):
+        """Set a new value to the desicion
+
+        Args:
+            decision (bool): new value
+        """
+        self._decision = decision
+    def toString(self):
+        """Resume the decision info into an string
+
+        Returns:
+            str: decision info
+        """
+        pass
+    def addDetail(self, key: str, value):
+        """Add a new detail to a decision, or update it if exists
+
+        Args:
+            key (str): detail name
+            value (any): detail value
+        """
+        pass
