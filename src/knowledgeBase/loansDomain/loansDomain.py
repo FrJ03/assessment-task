9454c8078f8906dd4ace52cf2dfd54d00a1aa1e2
Yo <franjomor@gmail.com>
2024-06-11 18:18:38 +0200

    feat: cases for loan domain

--- src/knowledgeBase/loansDomain/loansDomain.py
+++ src/knowledgeBase/loansDomain/loansDomain.py
@@ -1,4 +1,51 @@
 from knowledgeBase.commons.decision import Decision
+from knowledgeBase.commons.case import Case
+
+class Age(Case):
+    def __init__(self):
+        super(Age, self).__init__('edad', 'natural')
+
+class Amount(Case):
+    def __init__(self):
+        super(Amount, self).__init__('cuantía del cliente', 'real')
+
+class AnnualIncomes(Case):
+    def __init__(self):
+        super(AnnualIncomes, self).__init__('ingresos anuales', 'real')
+
+class Defaults(Case):
+    def __init__(self):
+        super(Defaults, self).__init__('número de impagos', 'natural')
+
+class Debt(Case):
+    def __init__(self):
+        super(Debt, self).__init__('deuda', 'real')
+
+class EmploymentStatus(Case):
+    def __init__(self):
+        super(EmploymentStatus, self).__init__('situación laboral', 'selectable', ['desempleado', 'temporal', 'indefinido', 'fijo', 'autónomo/empresario'])
+
+class LoanAmount(Case):
+    def __init__(self):
+        super(LoanAmount, self).__init__('cuantía del préstamo', 'real')
+
+class LoanDuration(Case):
+    def __init__(self):
+        super(LoanDuration, self).__init__('duración del préstamo', 'natural')
+
+
+def getCases():
+    cases=[]
+    cases.append(Age())
+    cases.append(Amount())
+    cases.append(AnnualIncomes())
+    cases.append(EmploymentStatus())
+    cases.append(Defaults())
+    cases.append(Debt())
+    cases.append(LoanAmount())
+    cases.append(LoanDuration())
+    return cases
+
 
 """Decision class for loans domain
 """
