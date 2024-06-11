b89be155a6604df8319e27d7344bb8f0e95a6c9e
Yo <franjomor@gmail.com>
2024-06-11 17:49:15 +0200

    feat: case documented

--- src/knowledgeBase/commons/case.py
+++ src/knowledgeBase/commons/case.py
@@ -1,5 +1,14 @@
+"""Class that contains a case info
+"""
 class Case():
     def __init__(self, name, type, allowedValues=None):
+        """_summary_
+
+        Args:
+            name (str): _description_
+            type (str): Type of the attribute: natural, real, selectable
+            allowedValues (array<str>, optional): Values allowed for selectable attributes. Defaults to None.
+        """
         self.name = name
         self.type = type
         self.allowedValues = allowedValues
