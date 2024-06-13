6c368441b9abe37bcbb7e228229f09f17bcd5626
Yo <franjomor@gmail.com>
2024-06-13 15:45:10 +0200

    feat: loan domain adapted

--- src/knowledgeBase/loansDomain/loansDomain.py
+++ src/knowledgeBase/loansDomain/loansDomain.py
@@ -77,21 +77,25 @@ class EndLoanCriteria(Criteria):
         """
         super(EndLoanCriteria, self).__init__('edad de finalizanción del préstamo')
 
-    def eval(self, case, decision: Decision):
+    def eval(self, case, value):
         """Redefinition of eval method
 
         Args:
             case (Array<(key,value)>): Case information
-            decision (Decision): Current decision
+            value (LoanResultValue): Current evaluation data
 
         Returns:
-            Decision: New decision
+            Array<(key, values>: Result values
+            str: evaluation message
         """
-        super(EncodingWarning, self).eval(case, decision)
+        super(EncodingWarning, self).eval(case, value)
 
         clientAge = -1
         loanDuration = -1
         message = f'evaluando {self.name}\n\t'
+
+        values = []
+
 
         for i in range(len(case)):
             if(case[i][0] == 'edad del cliente'):
@@ -100,18 +104,18 @@ class EndLoanCriteria(Criteria):
                 loanDuration = int(case[i][1])
         
         if(clientAge < 0 or loanDuration < 0):
-            decision.setDecision(False)
-            decision.setDecisionMade(True)
+            values.append(('decisión', False))
+            values.append(('decidido', True))
             message = '{message}No se ha especificado la edad del cliente o la duración del préstamo\n'
         elif (clientAge + loanDuration > 80):
-            decision.setDecision(False)
-            decision.setDecisionMade(True)
+            values.append(('decisión', False))
+            values.append(('decidido', True))
             message = '{message}Denegado debido a que la edad de finalización del préstamo es mayor a 80 años\n'
         else:
-            decision.setDecision(True)
+            values.append(('decisión', True))
             message = '{message}Edad de finalización del préstamo válida\n'
         
-        return decision, message
+        return values, message
     
 """Criteria for unemployed status
 """  
@@ -121,38 +125,41 @@ class UnemployedCriteria(Criteria):
         """
         super(UnemployedCriteria, self).__init__('cliente no desempleado')
     
-    def eval(self, case, decision: Decision):
+    def eval(self, case, value):
         """Redefinition of eval method
 
         Args:
             case (Array<(key,value)>): Case information
-            decision (Decision): Current decision
+            value (LoanResultValue): Current evaluation data
 
         Returns:
-            Decision: New decision
+            Array<(key, values>: Result values
+            str: evaluation message
         """
-        super(UnemployedCriteria, self).eval(case, decision)
+        super(UnemployedCriteria, self).eval(case, value)
 
         employmentStatus = -''
         message = f'evaluando {self.name}\n\t'
+
+        values = []
 
         for i in range(len(case)):
             if(case[i][0] == 'situación laboral'):
                 employmentStatus = case[i][1]
         
         if(employmentStatus == ''):
-            decision.setDecision(False)
-            decision.setDecisionMade(True)
+            values.append(('decisión', False))
+            values.append(('decidido', True))
             message = '{message}No se ha especificado la situación laboral del cliente\n'
         elif (employmentStatus == 'desempleado'):
-            decision.setDecision(False)
-            decision.setDecisionMade(True)
+            values.append(('decisión', False))
+            values.append(('decidido', True))
             message = '{message}Denegado debido a que el cliente se encuentra en situación de desempleo\n'
         else:
-            decision.setDecision(True)
+            values.append(('decisión', True))
             message = '{message}Situación laboral válida\n'
         
-        return decision, message
+        return values, message
 
 """Criteria for employment status
 """
@@ -162,52 +169,55 @@ class EmploymentStatusCriteria(Criteria):
         """
         super(EmploymentStatusCriteria, self).__init__('situación laboral del cliente')
     
-    def eval(self, case, decision: Decision):
+    def eval(self, case, value):
         """Redefinition of eval method
 
         Args:
             case (Array<(key,value)>): Case information
-            decision (Decision): Current decision
+            value (LoanResultValue): Current evaluation data
 
         Returns:
-            Decision: New decision
+            Array<(key, values>: Result values
+            str: evaluation message
         """
-        super(EmploymentStatusCriteria, self).eval(case, decision)
+        super(EmploymentStatusCriteria, self).eval(case, value)
 
         employmentStatus = -''
         message = f'evaluando {self.name}\n\t'
+
+        values = []
 
         for i in range(len(case)):
             if(case[i][0] == 'situación laboral'):
                 employmentStatus = case[i][1]
         
         if(employmentStatus == ''):
-            decision.setDecision(False)
-            decision.setDecisionMade(True)
+            values.append(('decisión', False))
+            values.append(('decidido', True))
             message = '{message}No se ha especificado la situación laboral del cliente\n'
         elif (employmentStatus == 'desempleado'):
-            decision.setDecision(False)
-            decision.setDecisionMade(True)
+            values.append(('decisión', False))
+            values.append(('decidido', True))
             message = '{message}Denegado debido a que el cliente se encuentra en situación de desempleo\n'
         elif(employmentStatus == 'fijo'):
-            decision.setDecision(True)
-            decision.addDetail('interés', 0.02)
+            values.append(('decisión', True))
+            values.append(('interés', 0.02))
             message = '{message}Situación laboral válida\n'
         elif(employmentStatus == 'indefinido' or employmentStatus == 'autónomo/empresario'):
-            decision.setDecision(True)
-            decision.addDetail('interés', 0.025)
+            values.append(('decisión', True))
+            values.append(('interés', 0.025))
             message = '{message}Situación laboral válida\n'
         elif(employmentStatus == 'temporal'):
-            decision.setDecision(True)
-            decision.addDetail('interés', 0.035)
+            values.append(('decisión', True))
+            values.append(('interés', 0.035))
             message = '{message}Situación laboral válida\n'
         else:
-            decision.setDecision(False)
-            decision.setDecisionMade(True)
+            values.append(('decisión', False))
+            values.append(('decidido', True))
             message = '{message}Situación laboral incorrecta\n'
 
         
-        return decision, message
+        return values, message
 
 """Criteria for the number of times that the client has not paid his debts
 """
@@ -217,38 +227,38 @@ class DefaultsCriteria(Criteria):
         """
         super(DefaultsCriteria, self).__init__('número de impagos')
     
-    def eval(self, case, decision: Decision):
+    def eval(self, case, value):
         """Redefinition of eval method
 
         Args:
             case (Array<(key,value)>): Case information
-            decision (Decision): Current decision
+            value (LoanResultValue): Current evaluation data
 
         Returns:
-            Decision: New decision
+            Array<(key, values>: Result values
+            str: evaluation message
         """
-        super(DefaultsCriteria, self).eval(case, decision)
+        super(DefaultsCriteria, self).eval(case, value)
 
         defaults = -1
         message = f'evaluando {self.name}\n\t'
-        interest = decision.getDetail('interest')
+
+        values = []
 
         for i in range(len(case)):
             if(case[i][0] == 'número de impagos'):
                 defaults = case[i][1]
         
         if(defaults == -1):
-            decision.setDecision(False)
-            decision.setDecisionMade(True)
+            values.append(('decisión', False))
+            values.append(('decidido', True))
             message = '{message}No se ha especificado el número de impagos\n'
         else:
-            interest = decision.getDetail('interest')
-
-            decision.setDecision(True)
-            decision.addDetail('interés', interest + (defaults * 0.003))
+            values.append(('decisión', True))
+            values.append(('interés', defaults * 0.003))
             message = '{message}Número de impagos evaluado\n'
 
-        return decision, message
+        return values, message
 
 """Criteria for the amount of money the client could request
 """
@@ -258,47 +268,57 @@ class AmountCriteria(Criteria):
         """
         super(AmountCriteria, self).__init__('cuantía solicitada')
     
-    def eval(self, case, decision: Decision):
+    def eval(self, case, value):
         """Redefinition of eval method
 
         Args:
             case (Array<(key,value)>): Case information
-            decision (Decision): Current decision
+            eval (LoanResultValue): Current evaluation data
 
         Returns:
-            Decision: New decision
+            Array<(key, values>: Result values
+            str: evaluation message
         """
-        super(AmountCriteria, self).eval(case, decision)
+        super(AmountCriteria, self).eval(case, value)
 
-        monthyPayment = decision.getDetail('cuantía mensual')
-        duration = decision.getDetail('duración')
+        duration = -1
         debts = -1
         annualIncomes = -1
+        amount = -1
+        interest = value.getValue('interés')
+
+        values = []
 
         for i in range(len(case)):
             if(case[i][0] == 'ingresos anuales'):
                 annualIncomes = case[i][1]
             elif(case[i][0] == 'deuda'):
                 debts = case[i][1]
+            elif(case[i][0] == 'duración del préstamo'):
+                duration = case[i][1]
+            elif(case[i][0] == 'cuantía del préstamo'):
+                amount = case[i][1]
 
-            if(debts != -1 and annualIncomes != -1):
+            if(debts != -1 and annualIncomes != -1 and duration != -1 and amount != -1):
                 break
+
+        monthyPayment = (amount + (amount * ((interest) + 1) ** duration)) / 12 * duration
         
         if(debts == -1 or annualIncomes == -1):
-            decision.setDecision(False)
-            decision.setDecisionMade(True)
+            values.append(('decisión', False))
+            values.append(('decidido', True))
             message = '{message}No se ha especificado el salario anual o la deuda actual\n'
         elif (monthyPayment * 12 * duration > 0.5 * ((annualIncomes * duration) - debts)):
-            decision.setDecision(False)
-            decision.setDecisionMade(True)
+            values.append(('decisión', False))
+            values.append(('decidido', True))
 
             message = '{message}Se ha solicitado demasiado dinero para sus posibilidades\n'
         else:
-            decision.setDecision(True)
+            values.append(('decisión', True))
 
             message = '{message}El préstamo entra dentro de sus posibilidades\n'
 
-        return decision, message
+        return values, message
     
 def getCriterias():
     """Function that returns all criterias
@@ -357,7 +377,29 @@ class LoanResultValue(ResultValue):
         duration = self._attributes[1][1]
         
         self._attributes[3][1] = (amount + (amount * ((interest) + 1) ** duration)) / 12 * duration
-            
+
+def setInitialResultValue(case):
+    """Initialize a LoanResultValue
+
+    Args:
+        case (Array<(key, values)>): Case info
+    
+    Return:
+        LoanResultValue: result value initizalized
+    """
+    duration = -1
+    amount = -1
+
+    for i in range(len(case)):
+        if(case[i][0] == 'duración del préstamo'):
+            duration = case[i][1]
+        elif(case[i][0] == 'cuantía del préstamo'):
+            amount = case[i][1]
+        
+        if(duration != -1 and amount != -1):
+            break
+    
+    return LoanResultValue([amount, duration, 0, 0])
 
 
 """Decision class for loans domain
