3e26f11feed85d1e2beebb20f7389d275cc60be2
Yo <franjomor@gmail.com>
2024-06-13 12:05:52 +0200

    feat: criteria modified

--- src/knowledgeBase/commons/criteria.py
+++ src/knowledgeBase/commons/criteria.py
@@ -8,14 +8,13 @@ class Criteria():
         """
         self.name = name
     
-    def eval(self, case, decision):
-        """Evaluate the criteria and return a decision based in the new criteria and the current decision
+    def eval(self, case):
+        """Evaluate the criteria and return a list of values modified based in the new criteria
 
         Args:
             case (Array<(name, value)>): Case information
-            decision (Decision): Current decision
         Returns:
-            Decision: new decision based on the criteria
+            Array<(key,value)>: new values based on the criteria
             str: decision message
         """
         pass
