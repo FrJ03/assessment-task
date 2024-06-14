02e064f826bf26b469d359a15bf2a0a9700a7fea
gabrielroncea7 <gabrielroncea00@gmail.com>
2024-06-14 15:02:06 +0200

    fix: recruitmentdomain fixed

--- src/knowledgeBase/recruitmentDomain/recruitmentDomain.py
+++ src/knowledgeBase/recruitmentDomain/recruitmentDomain.py
@@ -1,228 +1,268 @@
 from knowledgeBase.commons.decision import Decision
 from knowledgeBase.commons.case import Case
 from knowledgeBase.commons.criteria import Criteria
+from knowledgeBase.commons.resultValue import ResultValue
 
 """Nivel de estudios del candidato
 """
 class EducationLevel(Case):
     def __init__(self):
-        super(EducationLevel, self).__init__('nivel de estudios', 'selectable', ['secundaria', 'grado superior', 'universitario'])
+        super(EducationLevel, self).__init__('nivel de estudios', 'selectable', ['secundaria', 'grado medio', 'grado superior', 'universitario'])
 
 """Nivel de idioma del candidato
 """
-class LanguageProficiency(Case):
+class LanguageLevel(Case):
     def __init__(self):
-        super(LanguageProficiency, self).__init__('nivel de idioma', 'selectable', ['básico', 'intermedio', 'avanzado', 'nativo'])
+        super(LanguageLevel, self).__init__('nivel de idioma', 'selectable', ['básico', 'intermedio', 'avanzado'])
 
 """Experiencia laboral del candidato en años
 """
 class WorkExperience(Case):
     def __init__(self):
-        super(WorkExperience, self).__init__('experiencia laboral', 'natural')
-
-"""Puesto solicitado por el candidato
-"""
-class JobPosition(Case):
-    def __init__(self):
-        super(JobPosition, self).__init__('puesto solicitado', 'selectable', ['electricista', 'ingeniero eléctrico', 'otro'])
+        super(WorkExperience, self).__init__('experiencia laboral', 'selectable', ['SI', 'NO'])
 
 def getCases():
-    """Función que retorna los casos para el dominio de selección de personal
+    """Function that returns the cases for the recruitment domain
 
-    Returns:
-        Array<Case>: Lista de casos
-    """
-    cases = []
+        Returns:
+            Array<Case>: List of cases
+        """
+    cases=[]
     cases.append(EducationLevel())
-    cases.append(LanguageProficiency())
+    cases.append(LanguageLevel())
     cases.append(WorkExperience())
-    cases.append(JobPosition())
     
     return cases
 
-"""Criterio para evaluar el nivel de estudios del candidato
+"""Criteria for language level
 """
-class EducationCriteria(Criteria):
+class LanguageLevelCriteria(Criteria):
     def __init__(self):
-        """Constructor
-        """
-        super(EducationCriteria, self).__init__('nivel de estudios')
+        """Constructor"""
+        super(LanguageLevelCriteria, self).__init__('nivel de idioma')
+    
+    def eval(self, case, value):
+        """Redefinition of eval method"""
+        super(LanguageLevelCriteria, self).eval(case, value)
 
-    def eval(self, case, decision: Decision):
-        """Redefinición del método eval
+        languageLevel = ''
+        message = f'evaluando {self.name}\n\t'
+        values = []
 
-        Args:
-            case (Array<(key,value)>): Información del caso
-            decision (Decision): Decisión actual
+        for i in range(len(case)):
+            if(case[i][0] == 'nivel de idioma'):
+                languageLevel = case[i][1]
+        
+        if(languageLevel == ''):
+            values.append(('decisión', False))
+            values.append(('puntuación', 0))
+            message = f'{message}No se ha especificado el nivel de idioma\n'
+        elif (languageLevel == 'básico'):
+            values.append(('decisión', True))
+            values.append(('puntuación', 1))
+            message = f'{message}Nivel de idioma básico\n'
+        elif (languageLevel == 'intermedio'):
+            values.append(('decisión', True))
+            values.append(('puntuación', 2))
+            message = f'{message}Nivel de idioma intermedio\n'
+        elif (languageLevel == 'avanzado'):
+            values.append(('decisión', True))
+            values.append(('puntuación', 3))
+            message = f'{message}Nivel de idioma avanzado\n'
+        
+        return values, message
 
-        Returns:
-            Decision: Nueva decisión
-        """
-        super(EducationCriteria, self).eval(case, decision)
+class EducationLevelCriteria(Criteria):
+    def __init__(self):
+        """Constructor"""
+        super(EducationLevelCriteria, self).__init__('nivel de estudios')
+    
+    def eval(self, case, value):
+        """Redefinition of eval method"""
+        super(EducationLevelCriteria, self).eval(case, value)
 
         educationLevel = ''
-        jobPosition = ''
         message = f'evaluando {self.name}\n\t'
+        values = []
 
         for i in range(len(case)):
-            if case[i][0] == 'nivel de estudios':
+            if(case[i][0] == 'nivel de estudios'):
                 educationLevel = case[i][1]
-            elif case[i][0] == 'puesto solicitado':
-                jobPosition = case[i][1]
+        
+        if(educationLevel == ''):
+            values.append(('decisión', False))
+            values.append(('puntuación', 0))
+            message = f'{message}No se ha especificado el nivel de estudios\n'
+        elif (educationLevel == 'secundaria'):
+            values.append(('decisión', True))
+            values.append(('puntuación', 1))
+            message = f'{message}Nivel de estudios: secundaria\n'
+        elif (educationLevel == 'grado medio'):
+            values.append(('decisión', True))
+            values.append(('puntuación', 2))
+            message = f'{message}Nivel de estudios: grado medio\n'
+        elif (educationLevel == 'grado superior'):
+            values.append(('decisión', True))
+            values.append(('puntuación', 3))
+            message = f'{message}Nivel de estudios: grado superior\n'
+        elif (educationLevel == 'universitario'):
+            values.append(('decisión', True))
+            values.append(('puntuación', 4))
+            message = f'{message}Nivel de estudios: universitario\n'
+        
+        return values, message
 
-        if jobPosition == 'electricista':
-            if educationLevel == 'universitario':
-                decision.addDetail('puntuación', 10)
-                message = f'{message}Nivel universitario para puesto de electricista, puntuación 10\n'
-            elif educationLevel == 'grado superior':
-                decision.addDetail('puntuación', 7)
-                message = f'{message}Grado superior para puesto de electricista, puntuación 7\n'
-            elif educationLevel == 'secundaria':
-                decision.addDetail('puntuación', 4)
-                message = f'{message}Secundaria para puesto de electricista, puntuación 4\n'
-        else:
-            if educationLevel == 'universitario':
-                decision.addDetail('puntuación', 8)
-                message = f'{message}Nivel universitario, puntuación 8\n'
-            elif educationLevel == 'grado superior':
-                decision.addDetail('puntuación', 5)
-                message = f'{message}Grado superior, puntuación 5\n'
-            elif educationLevel == 'secundaria':
-                decision.addDetail('puntuación', 2)
-                message = f'{message}Secundaria, puntuación 2\n'
-
-        return decision, message
-
-"""Criterio para evaluar el nivel de idioma del candidato
-"""
-class LanguageCriteria(Criteria):
+class WorkExperienceCriteria(Criteria):
     def __init__(self):
-        """Constructor
-        """
-        super(LanguageCriteria, self).__init__('nivel de idioma')
+        """Constructor"""
+        super(WorkExperienceCriteria, self).__init__('experiencia laboral')
+    
+    def eval(self, case, value):
+        """Redefinition of eval method"""
+        super(WorkExperienceCriteria, self).eval(case, value)
 
-    def eval(self, case, decision: Decision):
-        """Redefinición del método eval
-
-        Args:
-            case (Array<(key,value)>): Información del caso
-            decision (Decision): Decisión actual
-
-        Returns:
-            Decision: Nueva decisión
-        """
-        super(LanguageCriteria, self).eval(case, decision)
-
-        languageProficiency = ''
+        workExperience = ''
         message = f'evaluando {self.name}\n\t'
+        values = []
 
         for i in range(len(case)):
-            if case[i][0] == 'nivel de idioma':
-                languageProficiency = case[i][1]
-
-        if languageProficiency == 'nativo':
-            decision.addDetail('puntuación', 10)
-            message = f'{message}Nivel de idioma nativo, puntuación 10\n'
-        elif languageProficiency == 'avanzado':
-            decision.addDetail('puntuación', 7)
-            message = f'{message}Nivel de idioma avanzado, puntuación 7\n'
-        elif languageProficiency == 'intermedio':
-            decision.addDetail('puntuación', 5)
-            message = f'{message}Nivel de idioma intermedio, puntuación 5\n'
-        elif languageProficiency == 'básico':
-            decision.addDetail('puntuación', 2)
-            message = f'{message}Nivel de idioma básico, puntuación 2\n'
-
-        return decision, message
-
-"""Criterio para evaluar la experiencia laboral del candidato
-"""
-class ExperienceCriteria(Criteria):
-    def __init__(self):
-        """Constructor
-        """
-        super(ExperienceCriteria, self).__init__('experiencia laboral')
-
-    def eval(self, case, decision: Decision):
-        """Redefinición del método eval
-
-        Args:
-            case (Array<(key,value)>): Información del caso
-            decision (Decision): Decisión actual
-
-        Returns:
-            Decision: Nueva decisión
-        """
-        super(ExperienceCriteria, self).eval(case, decision)
-
-        experience = -1
-        message = f'evaluando {self.name}\n\t'
-
-        for i in range(len(case)):
-            if case[i][0] == 'experiencia laboral':
-                experience = int(case[i][1])
-
-        if experience < 0:
-            decision.setDecision(False)
-            decision.setDecisionMade(True)
+            if(case[i][0] == 'experiencia laboral'):
+                workExperience = case[i][1]
+        
+        if(workExperience == ''):
+            values.append(('decisión', False))
+            values.append(('puntuación', 0))
             message = f'{message}No se ha especificado la experiencia laboral\n'
-        else:
-            decision.setDecision(True)
-            decision.addDetail('puntuación', experience * 2)
-            message = f'{message}Experiencia laboral evaluada, puntuación {experience * 2}\n'
+        elif (workExperience == 'SI'):
+            values.append(('decisión', True))
+            values.append(('puntuación', 1))
+            message = f'{message}Experiencia laboral: SI\n'
+        elif (workExperience == 'NO'):
+            values.append(('decisión', True))
+            values.append(('puntuación', 0))
+            message = f'{message}Experiencia laboral: NO\n'
+        
+        return values, message
 
-        return decision, message
 
 def getCriterias():
-    """Función que retorna todos los criterios
+    """Function that returns all criterias
     
     Returns:
-        Array<Criterias>: Todos los criterios
+        Array<Criterias>: All criterias
     """
     criterias = []
-    criterias.append(EducationCriteria())
-    criterias.append(LanguageCriteria())
-    criterias.append(ExperienceCriteria())
+    criterias.append(LanguageLevelCriteria())
+    criterias.append(EducationLevelCriteria())
+    criterias.append(WorkExperienceCriteria())
 
     return criterias
 
-"""Clase de decisión para el dominio de selección de personal
-"""
-class RecruitmentDecision(Decision):
-    """Constructor de la clase Decision
+class RecruitmentResultValue(ResultValue):
+    def __init__(self, values=[0, 0, 0]):
+        """Constructor
+
+        Args:
+            values (Array<any>, optional): initial values of the attributes. The order is: nivel de estudios -> nivel de idioma -> experiencia laboral. Defaults to [0, 0, 0]
+        """
+        attributes = [('nivel de estudios', values[0]), ('nivel de idioma', values[1]), ('experiencia laboral', values[2]), ('decisión', True), ('decidido', False)]
+
+        super(RecruitmentResultValue, self).__init__(attributes)
+    
+    def setValue(self, key, value):
+        """Add or set the value. It depends of the key
+
+        Args:
+            key (str): Attribute key. Could be 'nivel de estudios', 'nivel de idioma', 'experiencia laboral', 'decisión' or 'decidido'
+            value (any): Value to add or set
+        """
+        super().setValue(key, value)
+
+        if key == 'nivel de estudios':
+            self._attributes[0] = (self._attributes[0][0], value)
+        elif key == 'nivel de idioma':
+            self._attributes[1] = (self._attributes[1][0], value)
+        elif key == 'experiencia laboral':
+            self._attributes[2] = (self._attributes[2][0], value)
+        elif key == 'decisión':
+            self._attributes[3] = (self._attributes[3][0], value)
+        elif key == 'decidido':
+            self._attributes[4] = (self._attributes[4][0], value)
+
+def setInitialResultValue(case):
+    """Initialize a RecruitmentResultValue
 
     Args:
-        decision (bool, optional): Valor que representa la decisión. Defaults to False.
-    """
-    def __init__(self, decision: bool = False):
-        super(RecruitmentDecision, self).__init__(decision, [('puntuación', 0)])
+        case (Array<(key, values)>): Case info
     
-    """Sobrescribe el método toString de la clase Decision
+    Return:
+        RecruitmentResultValue: result value initialized
     """
+    educationLevel = -1
+    languageLevel = -1
+    workExperience = -1
+
+    for key, value in case:
+        if key == 'nivel de estudios':
+            if value == 'secundaria':
+                educationLevel = 1
+            elif value == 'grado medio':
+                educationLevel = 2
+            elif value == 'grado superior':
+                educationLevel = 3
+            elif value == 'universitario':
+                educationLevel = 4
+        elif key == 'nivel de idioma':
+            if value == 'básico':
+                languageLevel = 1
+            elif value == 'intermedio':
+                languageLevel = 2
+            elif value == 'avanzado':
+                languageLevel = 3
+        elif key == 'experiencia laboral':
+            if value == 'SI':
+                workExperience = 1
+            elif value == 'NO':
+                workExperience = 0
+
+    return RecruitmentResultValue([educationLevel, languageLevel, workExperience])
+
+
+"""Decision class for recruitment domain
+"""
+class RecruitmentDecision(Decision):
+    """Decision class constructor"""
+    def __init__(self, decision: bool = False):
+        super(RecruitmentDecision, self).__init__(decision, [('nivel de estudios', 0), ('nivel de idioma', 0), ('experiencia laboral', 0), ('puntuación total', 0)])
+
     def toString(self):
         if self._decision:
             message = 'Decisión: Aprobado\n\n'
-            for key, value in self._details:
-                message += f'\t{key}: {value}\n'
         else:
-            message = 'Decisión: Rechazado\n'
-        return message
-    
-    def addDetail(self, key: str, value):
-        super().addDetail(key, value)
-
-        self.recalcTotalScore()
-
-    def recalcTotalScore(self):
-        totalScore = 0
-
+            message = 'Decisión: Rechazado\n\n'
         for key, value in self._details:
-            if key == 'puntuación':
-                totalScore += value
+            message += f'{key} : {value}\n'
+        return message
 
-        # Actualiza la puntuación total en los detalles
-        for i in range(len(self._details)):
-            if self._details[i][0] == 'puntuación':
-                self._details[i][1] = totalScore
-                break
+def loadResultValue(value: ResultValue):
+    decision = RecruitmentDecision()
 
+    education_level = value.getValue('nivel de estudios')
+    language_level = value.getValue('nivel de idioma')
+    work_experience = value.getValue('experiencia laboral')
+
+    total_score = education_level + language_level + work_experience
+
+    decision.addDetail('nivel de estudios', education_level)
+    decision.addDetail('nivel de idioma', language_level)
+    decision.addDetail('experiencia laboral', work_experience)
+    decision.addDetail('puntuación total', total_score)
+
+    if total_score > 3:
+        decision.setDecision(True)
+    else:
+        decision.setDecision(False)
+
+    decision.setDecisionMade(True)
+
+    return decision
